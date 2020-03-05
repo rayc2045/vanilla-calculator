aed63bf7e929292d6111a1561a5fb4083d5b108d
Ray Chang <raychang2017@gmail.com>
2020-03-05 20:15:37 +0800

    修復算式間開頭為零的 bug

--- js/vanilla.js
+++ js/vanilla.js
@@ -76,7 +76,8 @@ class Calculator {
 		if (e.target.textContent === '☞') return this.clearOne();
 		if (e.target.textContent === '=') return this.equals();
 		
-		this.replaceOldOperator(e)
+		this.preventZeroBug(e);
+		this.replaceOldOperator(e);
 		this.showInput(e.target.textContent);
 		try {
 			this.showResult();
@@ -84,6 +85,34 @@ class Calculator {
 			console.log(err + ':\n使用 eval() 計算時，算式最後為加減乘除會報錯。');
 		}
 		this.fontSizeAdjust();
+	}
+
+	preventZeroBug(e) {
+		const lastChar = this.inputArray[this.inputArray.length - 1];
+		const lastSecondChar = this.inputArray[this.inputArray.length - 2];
+		
+		// Example: 9 X 0(2)
+		if (
+			e.target.textContent === 'O' ||
+			e.target.textContent === '1' ||
+			e.target.textContent === '2' ||
+			e.target.textContent === '3' ||
+			e.target.textContent === '4' ||
+			e.target.textContent === '5' ||
+			e.target.textContent === '6' ||
+			e.target.textContent === '7' ||
+			e.target.textContent === '8' ||
+			e.target.textContent === '9'
+		) {
+			if (
+				(lastChar === '0' && lastSecondChar === ' ÷ ') ||
+				(lastChar === '0' && lastSecondChar === ' X ') ||
+				(lastChar === '0' && lastSecondChar === ' – ') ||
+				(lastChar === '0' && lastSecondChar === ' ＋ ') ||
+				(lastChar === '0' && lastSecondChar === '%')
+			)
+				this.inputArray.length -= 1;
+		}
 	}
 
 	replaceOldOperator(e) {
@@ -180,7 +209,7 @@ class Calculator {
 			.replace(/[^-()\d/*+.]/g, ''); // 無視加減乘除符號才能讓 eval 函數計算字串（全局替換 http://www.w3school.com.cn/jsref/jsref_replace.asp）
 
 		const resultNumber = Math.round(eval(calculatableString) * 1e12) / 1e12; // 小數點留到第 12 位以確保計算準確，可輸入 0.1 + 0.2 測試（不精確計算 https://zh.javascript.info/number#bu-jing-que-ji-suan）
-
+		
 		// const resultNumber = +eval(calculatableString).toFixed(12); // 相較於 Math.round()，toFixed() 存在精度缺失（https://zh.javascript.info/number#wei-shi-mo-6-35tofixed-1-6-3）；此外 toFixed() 會將資料轉成字串，如果不再轉為數字，將產生 .000000000000
 
 		this.resultElement.textContent = this.thousandFormat(resultNumber);
